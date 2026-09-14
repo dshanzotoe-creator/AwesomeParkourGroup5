668a46c72d7f027a8b0349edb522f47397dd48fd
FGDore <emil.rotmark@edu.futuregames.se>
2026-09-14 13:44:53 +0200

    Added moving pickup and destroy on pickup

--- Assets/Scripts/Pickup/Pickup.cs
+++ Assets/Scripts/Pickup/Pickup.cs
@@ -1,16 +1,61 @@
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(PickupMovement))]
 public class Pickup : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    #region variables
+    [SerializeField] Type type;
+    GameObject player;
+    PickupMovement pickupMovement;
+    float distanceToPlayer = 0;
+    float minimumDistance = 0; // to player
+    bool pickedUp = false;
+    #endregion
+
     void Start()
     {
-        
+        player = GameObject.Find("Player");
+        pickupMovement = GetComponent<PickupMovement>();
+        switch (type)
+        {
+            case Type.idle:
+                minimumDistance = 0;
+                break;
+            case Type.moving:
+                minimumDistance = 7.5f;
+                StartCoroutine(CheckDistance());
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HandlePickup()
     {
-        
+        Destroy(gameObject);
+    }
+
+    IEnumerator CheckDistance()
+    {
+        while (!pickedUp)
+        {
+            distanceToPlayer = Vector3.Distance(gameObject.transform.position, player.transform.position);
+            if (!pickupMovement.IsMovingFromPlayer() && distanceToPlayer < minimumDistance)
+            {
+                pickupMovement.StartMovingFromPlayer();
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public bool InRange()
+    {
+        if (distanceToPlayer < minimumDistance) return true;
+        return false;
+    }
+
+    enum Type
+    {
+        idle,
+        moving
     }
 }
