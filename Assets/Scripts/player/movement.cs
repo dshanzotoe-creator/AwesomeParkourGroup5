17bbe1c54c6dd81164402ddd42f677925d08412e
dshanzotoe <darrenscotthanzotoe@gmail.com>
2026-09-16 14:23:10 +0200

    final movement push

--- Assets/Scripts/player/movement.cs
+++ Assets/Scripts/player/movement.cs
@@ -40,8 +40,8 @@ public class Movement : MonoBehaviour
         
     }
 
-    // LateUpdate is called once per frame, after all Update functions have been called
-    void LateUpdate() 
+    //Update is called once per frame
+    void Update() 
     {
         
 
