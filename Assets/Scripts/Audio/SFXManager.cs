f5e967a3e0637c4b70601f3628236dfeb9b2ae1f
SimonStarburst <326016527+SimonStarburst@users.noreply.github.com>
2026-09-18 18:42:40 +0200

    Updated SFXManager script
    Took inspiration from a script a team member shared with me and started changing SFXManager script to work with Player.

--- Assets/Scripts/Audio/SFXManager.cs
+++ Assets/Scripts/Audio/SFXManager.cs
@@ -7,12 +7,6 @@ public class SFXManager : MonoBehaviour
     [SerializeField] private AudioSource sfxObject;
     [SerializeField] private AudioClip[] audioClips;
 
-    private float clipLength;
-    private float timer = 0f;
-
-    public float lowPitchRange = .95f;
-    public float highPitchRange = 1.05f;
-
     private void Awake()
     {        
         DontDestroyOnLoad(gameObject);
@@ -26,11 +20,6 @@ public class SFXManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-    }
-
-    private void Update()
-    {
-        timer += Time.deltaTime;
     }
 
     public void PlaySFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
@@ -54,38 +43,47 @@ public class SFXManager : MonoBehaviour
         Destroy(audioSource.gameObject, clipLength);
     }
 
-    public void PlayRandomSFXClip(AudioClip[] audioClip, Transform spawnTransform, float volume)
+    public void PlaySFXClipRandomPitch(AudioClip audioClip, Transform spawnTransform, float volume)
     {
-        if (timer >= clipLength)
+        if (audioClip == null) return;
+
+        // Spawn in gameObject
+        AudioSource audioSource = Instantiate(sfxObject, spawnTransform.position, Quaternion.identity);
+
+        // Assign audioClip randomly selected thanks to the int rand above
+        audioSource.clip = audioClip;
+
+        // Assign volume
+        audioSource.volume = volume;
+
+        // Assign random pitch to played audioClip
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+
+        // Play sound, might change from PlayOneShot to just Play() in the future since the if-statement theoretically stops all the sounds from playing at once
+        audioSource.Play();
+
+        // Changes clipLength to the length of the audioSource
+        float clipLength = audioSource.clip.length;
+
+        // Destroy SFX after it's done playing
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    public float GetClipLength(AudioClip clip)
+    {
+        return clip.length;
+    }
+
+    public AudioClip GetAudioClip(string clipToPlay)
+    {
+        foreach (AudioClip clip in audioClips)
         {
-            // Assign a random index
-            int rand = Random.Range(0, audioClip.Length);
-
-            // Slightly changes the pitch of the random sound to create variety
-            float randomPitch = Random.Range(lowPitchRange, highPitchRange);
-
-            // Spawn in gameObject
-            AudioSource audioSource = Instantiate(sfxObject, spawnTransform.position, Quaternion.identity);
-
-            // Assign audioClip randomly selected thanks to the int rand above
-            audioSource.clip = audioClip[rand];
-
-            // Assign random pitch to played audioClip
-            audioSource.pitch = randomPitch;
-
-            // Assign volume
-            audioSource.volume = volume;
-
-            // Play sound, might change from PlayOneShot to just Play() in the future since the if-statement theoretically stops all the sounds from playing at once
-            audioSource.PlayOneShot(audioSource.clip);
-
-            // Changes clipLength to the length of the audioSource
-            clipLength = audioSource.clip.length;
-
-            // Destroy SFX after it's done playing
-            Destroy(audioSource.gameObject, clipLength);
-
-            timer = 0f;
+            if (clip.name == clipToPlay)
+            {
+                return clip;
+            }
         }
+        Debug.Log("No sound found");
+        return null;
     }
 }
