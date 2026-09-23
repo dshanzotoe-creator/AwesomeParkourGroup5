090e55a2c45a3848c00ae58bea19b65b84dd31a4
FGDore <emil.rotmark@edu.futuregames.se>
2026-09-23 13:30:54 +0200

    Added a basic scoring system

--- Assets/Scripts/UI/WorldTImer.cs
+++ Assets/Scripts/UI/WorldTImer.cs
@@ -1,12 +1,15 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(WorldScore))]
 public class WorldTimer : MonoBehaviour
 {
 
     public Text timeText;
     private float time;
+    [SerializeField] private float timeMax;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,15 +20,17 @@ public class WorldTimer : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
+        int timeMaxInMinutes = Mathf.RoundToInt(timeMax/60);
         int minutes = Mathf.FloorToInt(time / 60);
         int seconds = Mathf.FloorToInt(time % 60);
         int milliseconds = Mathf.FloorToInt((time * 1000) % 1000);
 
         timeText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
 
-        if(minutes >= 20)
+        if(minutes >= timeMaxInMinutes)
         {
-            Application.Quit();
+            GetComponent<WorldScore>().SetScore(time);
+            SceneManager.LoadScene(0);
         }
     }
 }
