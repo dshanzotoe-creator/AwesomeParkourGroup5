5426a12e9050c31acb46be03a8530cbb2cec4753
Luxecore <lucaspontus@gmail.com>
2026-09-24 13:22:47 +0200

    High score
    We now have a working highscoring that saves the data with playerprefs

--- Assets/Scripts/UI/WorldTImer.cs
+++ Assets/Scripts/UI/WorldTImer.cs
@@ -1,4 +1,3 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,31 +5,51 @@ using UnityEngine.UI;
 [RequireComponent(typeof(WorldScore))]
 public class WorldTimer : MonoBehaviour
 {
-
     public Text timeText;
     private float time;
+
     [SerializeField] private float timeMax;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private WorldScore worldScore;
+
     void Start()
     {
-       
+        worldScore = GetComponent<WorldScore>();
+
+        timeMax = 300;
     }
 
     void Update()
     {
         time += Time.deltaTime;
-        int timeMaxInMinutes = Mathf.RoundToInt(timeMax/60);
+
         int minutes = Mathf.FloorToInt(time / 60);
         int seconds = Mathf.FloorToInt(time % 60);
         int milliseconds = Mathf.FloorToInt((time * 1000) % 1000);
 
-        timeText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+        timeText.text = string.Format(
+            "{0:00}:{1:00}:{2:000}",
+            minutes,
+            seconds,
+            milliseconds
+        );
 
-        if(minutes >= timeMaxInMinutes)
+        float remainingTime = Mathf.Max(0, timeMax - time);
+
+        GetComponent<WorldScore>().SetScore(remainingTime);
+
+        if (time >= timeMax)
         {
-            GetComponent<WorldScore>().SetScore(time);
             SceneManager.LoadScene(0);
         }
+    }
+
+    public void Finish()
+    {
+        float remainingTime = Mathf.Max(0, timeMax - time);
+
+        worldScore.SetScore(remainingTime);
+
+        SceneManager.LoadScene(0);
     }
 }
